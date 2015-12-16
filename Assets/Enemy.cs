678c0c3b36333dc4dd1bca3b6514b37b466e7aa5
Politko <pajlot@gmail.com>
2015-12-16 22:39:09 +0100

    Enemy now is followoing Player.

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,12 +4,20 @@ using System.Collections.Generic;
 
 public class Enemy : MonoBehaviour {
 
-    public List<GameObject> wayPoints = new List<GameObject>();
+    public List<WayPoint> wayPoints = new List<WayPoint>();
     public float wayPointTime = 2;
+    public float moveSpeed = 5;
+    public Rigidbody2D rb;
 
     private float currentWayPointTime = 0;
     private int currentWaypointIndex = 0;
     private bool destinationReached = false;
+
+    public GameObject target = null;
+
+    void Awake() {
+        rb = GetComponent<Rigidbody2D>() as Rigidbody2D;
+    }
 
 	// Use this for initialization
 	void Start () {
@@ -18,24 +26,53 @@ public class Enemy : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        //Debug.Log(Vector3.Distance(transform.position, wayPoints[currentWaypointIndex].transform.position));
 
-        if(!destinationReached) {
-            if(Vector3.Distance(transform.position, wayPoints[currentWaypointIndex].transform.position) > 0.41f) {
-                transform.position = Vector3.Lerp(transform.position, wayPoints[currentWaypointIndex].transform.position, Time.deltaTime);
-            } else {
-                transform.position = wayPoints[currentWaypointIndex].transform.position;
-                destinationReached = true;
-                currentWaypointIndex = (++currentWaypointIndex) % wayPoints.Count;
-            }
+        if(target != null) {
+            Vector2 direction = target.transform.position - transform.position;
+            direction.Normalize();
+            rb.velocity = direction * moveSpeed;
         } else {
-            currentWayPointTime += Time.deltaTime;
+            if(!destinationReached) {
+                if(Vector3.Distance(transform.position, wayPoints[currentWaypointIndex].transform.position) > 0.1f) {
+                    //transform.position = Vector3.Lerp(transform.position, wayPoints[currentWaypointIndex].transform.position, Time.deltaTime);
+                    Vector2 direction = wayPoints[currentWaypointIndex].transform.position - transform.position;
+                    direction.Normalize();
 
-            if(currentWayPointTime >= wayPointTime) {
-                destinationReached = false;
-                currentWayPointTime = 0;
+                    rb.velocity = direction * moveSpeed;
+                } else {
+                    transform.position = wayPoints[currentWaypointIndex].transform.position;
+                    wayPointTime = wayPoints[currentWaypointIndex].waitTime;
+
+                    destinationReached = true;
+                    currentWaypointIndex = (++currentWaypointIndex) % wayPoints.Count;
+                }
+            } else {
+                currentWayPointTime += Time.deltaTime;
+
+                //Debug.Log(currentWayPointTime);
+
+                if(currentWayPointTime >= wayPointTime) {
+                    destinationReached = false;
+                    currentWayPointTime = 0;
+                }
             }
         }
+        //Debug.Log(Vector3.Distance(transform.position, wayPoints[currentWaypointIndex].transform.position));
+    }
 
-	}
+    void OnTriggerEnter2D(Collider2D other) {
+        Player player = other.GetComponent<Player>() as Player;
+
+        if(player != null) {
+            target = player.gameObject;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        Player player = other.GetComponent<Player>() as Player;
+
+        if(player != null) {
+            target = null;
+        }
+    }
 }
